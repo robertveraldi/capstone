12dde6c815deb96e046a83e12137005c89997763
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-08 12:48:43 -0500

    exercises index function, functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Header } from "./Header";
 import { Home } from "./Home";
 import { Footer } from "./Footer";
+import { ExercisesIndex } from "./ExercisesIndex";
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <ExercisesIndex />
       <Footer />
     </div>
   );
---
+++ src/ExercisesIndex.jsx
@@ -0,0 +1,7 @@
+export function ExercisesIndex(props) {
+  return (
+    <div>
+      <h1>All Exercises</h1>
+    </div>
+  );
+}
