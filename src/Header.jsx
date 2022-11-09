e21560358c50d090a95b8a60f2d4dad57609e99b
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-09 13:03:02 -0500

    website name header

--- src/Header.jsx
+++ src/Header.jsx
@@ -14,6 +14,7 @@ export function Header() {
   return (
     <header>
       <nav>
+        <h1>Gym Routine App</h1>
         <Link to="/">Home</Link> |{" "}
         {localStorage.jwt === undefined ? (
           <>
