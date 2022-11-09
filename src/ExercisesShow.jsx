f60d55a50d02bd247599249fa2d66050bf23eb87
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-09 12:58:49 -0500

    removed 'exercise' header from exercise show, heading is now exercise name

--- src/ExercisesShow.jsx
+++ src/ExercisesShow.jsx
@@ -1,8 +1,7 @@
 export function ExercisesShow(props) {
   return (
     <div className="exercises-show">
-      <h1>Exercise</h1>
-      <h3>{props.exercise.name}</h3>
+      <h2>{props.exercise.name}</h2>
       <p>{props.exercise.description}</p>
       <img src={props.exercise.image_url} />
       <p>{props.exercise.video_url}</p>
