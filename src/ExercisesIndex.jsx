6836623b35937ea61752086155e13d806f88dd6c
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-09 13:43:30 -0500

    video is now a hyperlink

--- src/ExercisesIndex.jsx
+++ src/ExercisesIndex.jsx
@@ -1,3 +1,5 @@
+import { Player } from "video-react";
+
 export function ExercisesIndex(props) {
   console.log(props.exercises, "im here");
   return (
@@ -8,7 +10,9 @@ export function ExercisesIndex(props) {
           <h2>{exercise.name}</h2>
           <p>{exercise.description}</p>
           <img src={exercise.image_url} />
-          <p>{exercise.video_url}</p>
+          {/* <p>{exercise.video_url}</p> */}
+          <a href={exercise.video_url}>Video</a>
+          {/* <Player playsInline src={exercise.video_url} /> */}
           <button onClick={() => props.onSelectExercise(exercise)}>More Info</button>
           <button>Add To Routine</button>
         </div>
