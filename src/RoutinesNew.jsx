a151e810d07fdc0653c55c896332b090abfd1fa8
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-10 14:42:51 -0500

    reverted back to manually input data using exercise id

--- src/RoutinesNew.jsx
+++ src/RoutinesNew.jsx
@@ -1,14 +1,27 @@
+import axios from "axios";
+
 export function RoutinesNew(props) {
-  const handleCreateRoutineSubmit = (event) => {
+  const handleCreateRoutine = (params, successCallback) => {
+    console.log("handleCreateRoutine", params);
+    axios.post("http://localhost:3000/routines.json", params).then((response) => {
+      successCallback();
+    });
+  };
+
+  const handleSubmit = (event) => {
     event.preventDefault();
     const params = new FormData(event.target);
-    props.onCreateRoutine(params, () => event.target.reset());
+    handleCreateRoutine(params, () => event.target.reset());
+    // for (const pair of params.entries()) {
+    //   console.log(`${pair[0]}, ${pair[1]}`);
+    // }
   };
+  console.log(props.onCreateRoutine);
 
   return (
     <div>
       <h1>Add to Your Routine</h1>
-      <form onSubmit={handleCreateRoutineSubmit}>
+      <form onSubmit={handleSubmit}>
         <div>
           Exercise ID: <input name="exercise_id" type="number" />
         </div>
@@ -21,3 +34,21 @@ export function RoutinesNew(props) {
   );
 }
 // this form needs to have a dropdown with exercise names, box for reps input
+
+// properties of a select element
+
+{
+  /* {exercises.map((exercise) => (
+        <div key={exercise}>
+          <p>{exercise.name}</p>
+        </div> */
+}
+{
+  /* ))} */
+}
+
+// <label for="exer">Exercises: </label>
+// <select name="exercises" id="exer">
+//   <option value="select">Select Exercise</option>
+//   <option value="Barbell Bench Press">Barbell Bench Press</option>
+// </select>
