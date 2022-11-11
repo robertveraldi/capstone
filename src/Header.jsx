5adf91af382e8929afecffbe3d05a5fadd5af87a
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-11 13:10:19 -0500

    header bootstrap navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -13,23 +13,57 @@ export function Header() {
   // };
 
   return (
-    <header>
-      <nav>
-        <h1>Gym Routine App</h1>
-        <Link to="/">Home</Link> |{" "}
-        {localStorage.jwt === undefined ? (
-          <>
-            <Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
-          </>
-        ) : (
-          // <Link to="/logout">Logout</Link>
-          <>
-            <Link to="/your_routine">Your Routine</Link> | <Link to="/add_exercise">Add to Your Routine</Link> |{" "}
-            <LogoutLink />
-          </>
-        )}
-        {/* this doesnt need to be a new page, just have the link work */}
-      </nav>
-    </header>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Gym Routine
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/">
+                Home
+              </Link>
+            </li>
+            {localStorage.jwt === undefined ? (
+              <>
+                <Link className="nav-link active" to="/login">
+                  Login
+                </Link>
+                <Link className="nav-link active" to="/signup">
+                  Signup
+                </Link>
+              </>
+            ) : (
+              // <Link to="/logout">Logout</Link>
+              <>
+                <Link className="nav-link active" to="/your_routine">
+                  Your Routine
+                </Link>
+                <Link className="nav-link active" to="/add_exercise">
+                  Add to Your Routine
+                </Link>
+
+                <li className="nav-item">
+                  <LogoutLink />
+                </li>
+              </>
+            )}
+            {/* this doesnt need to be a new page, just have the link work */}
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
