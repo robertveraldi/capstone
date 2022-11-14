d12b5cdfa8bf8bd0095cfca3842bf2d64c00fcf8
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-14 17:05:34 -0500

    footer has close and video button. video button not yet functional

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -4,13 +4,42 @@ export function Modal(props) {
   if (props.show) {
     return (
       <div className="modal-background">
-        <section className="modal-main">
-          {props.children}
-          <button className="close" type="button" onClick={props.onClose}>
-            &#x2715;
-          </button>
-        </section>
+        <div className="modal-dialog modal-main">
+          <div className="modal-content">
+            {/* <div className="modal-header">
+              <h5 className="modal-title">Title</h5>
+            </div> */}
+            <div className="modal-body">{props.children}</div>
+            {console.log(props.children, "children")}
+            <div className="modal-footer">
+              <button type="button" className="btn btn-primary">
+                Video
+              </button>
+              &nbsp;
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={props.onClose}>
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+{
+  /* <div className="modal-background">
+<section className="modal-main">
+  {props.children}
+  <button className="close" type="button" onClick={props.onClose}>
+    &#x2715;
+  </button>
+</section>
+</div> */
+}
+
+// <p>{props.exercise.description}</p>
+// <img src={props.exercise.image_url} />
+// <a className="btn btn-primary" href={props.exercise.video_url} target="_blank" role="button">
+//   Video
+// </a>
