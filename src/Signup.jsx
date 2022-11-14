216ff1201b048bcdbfef6b10b88d18193a4f455b
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-11-14 17:11:43 -0500

    signup form centered on page

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -23,68 +23,74 @@ export function Signup() {
 
   return (
     <div id="signup">
-      <h1>Signup</h1>
+      <h1 className="text-center">Signup</h1>
       <ul>
         {errors.map((error) => (
           <li key={error}>{error}</li>
         ))}
       </ul>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label for="signupName" className="form-label">
-            Name
-          </label>
-          <div className="form-outline w-25">
-            <input name="name" type="text" className="form-control" id="signupName" placeholder="John Doe" />
+      <div className="container h-100">
+        <div className="row h-100 justify-content-center align-items-center">
+          <div className="col-10 col-md-8 col-lg-6">
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label for="signupName" className="form-label">
+                  Name
+                </label>
+                <div className="form-outline w-100">
+                  <input name="name" type="text" className="form-control" id="signupName" placeholder="John Doe" />
+                </div>
+              </div>
+              <div className="mb-3">
+                <label for="signupEmail" className="form-label">
+                  Email Address
+                </label>
+                <div className="form-outline w-100">
+                  <input
+                    name="email"
+                    type="email"
+                    className="form-control"
+                    id="signupEmail"
+                    placeholder="email@example.com"
+                    aria-describedby="email-help"
+                  />
+                </div>
+                <div id="emailHelp" className="form-text">
+                  We'll never share your email with anyone else.
+                </div>
+              </div>
+              <label for="signupPassword" className="form-label">
+                Password
+              </label>
+              <div className="form-outline w-100">
+                <input
+                  name="password"
+                  type="password"
+                  className="form-control"
+                  id="signupPassword"
+                  placeholder="Enter Password"
+                />
+              </div>
+              <label for="signupPasswordConfirm" className="form-label">
+                Password
+              </label>
+              <div className="form-outline w-100">
+                <input
+                  name="password_confirmation"
+                  type="password"
+                  className="form-control"
+                  id="signupPasswordConfirm"
+                  placeholder="Enter Password Again"
+                />
+              </div>
+              <br></br>
+              <button type="submit" className="btn btn-primary">
+                Signup
+              </button>
+            </form>
           </div>
         </div>
-        <div className="mb-3">
-          <label for="signupEmail" className="form-label">
-            Email Address
-          </label>
-          <div className="form-outline w-25">
-            <input
-              name="email"
-              type="email"
-              className="form-control"
-              id="signupEmail"
-              placeholder="email@example.com"
-              aria-describedby="email-help"
-            />
-          </div>
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
-        </div>
-        <label for="signupPassword" className="form-label">
-          Password
-        </label>
-        <div className="form-outline w-25">
-          <input
-            name="password"
-            type="password"
-            className="form-control"
-            id="signupPassword"
-            placeholder="Enter Password"
-          />
-        </div>
-        <label for="signupPasswordConfirm" className="form-label">
-          Password
-        </label>
-        <div className="form-outline w-25">
-          <input
-            name="password_confirmation"
-            type="password"
-            className="form-control"
-            id="signupPasswordConfirm"
-            placeholder="Enter Password Again"
-          />
-        </div>
-        <br></br>
-        <button type="submit" className="btn btn-primary">
-          Signup
-        </button>
-      </form>
+      </div>
     </div>
   );
 }
